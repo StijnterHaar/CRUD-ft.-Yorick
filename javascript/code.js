80fc3cc9dcc202222be27164e83f3f00e8dc9847
Yorinio <95612001+Yorinio@users.noreply.github.com>
2022-05-12 23:58:05 +0200

    Contactformulier pagina toegevoegd en kleine aanpassingen aan design

---
+++ javascript/code.js
@@ -0,0 +1,14 @@
+// Fade in elements
+function fadeIn(element, transitionTime, delayInMs) {
+    setTimeout(
+        function() {
+            document.querySelector(element).style.transition = transitionTime;
+            document.querySelector(element).style.opacity = "1";
+        }, delayInMs);
+}
+
+// Run when page loaded
+window.onload = function() {
+    fadeIn("html", "0.5s", 1 );
+    fadeIn(".coverlabel", "3s", 1 );
+};
