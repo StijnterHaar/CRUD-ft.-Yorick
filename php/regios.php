c346ddbb8879d98352e91687a0cb2ce57c403243
Yorinio <95612001+Yorinio@users.noreply.github.com>
2022-06-08 13:52:25 +0200

    php

---
+++ php/regios.php
@@ -0,0 +1,16 @@
+<?php
+
+
+     
+                $sql2 = "SELECT DISTINCT * FROM `regio`";
+                $stmt2 = $connect->prepare($sql2);
+                $stmt2->execute();
+
+                $producten = $stmt2->fetchAll();
+                foreach ($producten as $product) {
+                    ?> <li><?php echo $product['stad']; ?>, <?php echo $product['land']; ?> </a></li> <?php
+                }
+
+                
+            
+            ?> 
